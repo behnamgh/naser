2edf6962d0d85bf1df4b9790ab6eea580dde803a
Behnam Ghafary <behnam.ghafary@gmail.com>
2020-02-07 18:24:13 +0330

    initialization

---
+++ packages/client/src/App.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import HttpClient from './utils/HttpClient';
+
+function useCalendars() {
+  const [calendars, setCalendar] = useState([]);
+
+  useEffect(() => {
+    HttpClient
+      .get('/rest/calendars')
+      .then((calendars) => {
+        setCalendar(calendars);
+      });
+  }, []);
+
+  return [calendars, setCalendar];
+}
+
+function App() {
+  const [calendars] = useCalendars();
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>Speakers agenda</h3>
+        <table>
+          <tbody>
+            {
+              calendars.map((calendar) => {
+                return (
+                  <tr key={calendar.id}>
+                    <td>{calendar.id}</td>
+                    <td>{calendar.name}</td>
+                  </tr>
+                );
+              })
+            }
+          </tbody>
+        </table>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Naser
+
+This project for presenting Naser - Sone of Man game into world.
+
+## Instruction
+
+We use Lerna for having our client and server and makes them into mono repository.
+Dcoker and pm2 are the other technologies that we use.
+
+### Start
+
+with "yarn start" you can start the project
