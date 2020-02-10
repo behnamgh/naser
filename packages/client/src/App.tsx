d85d0099ace769e4062f0011b9f37f6b7a1c9fb1
Behnam Ghafary <behnam.ghafary@gmail.com>
2020-02-10 18:30:58 +0330

    convert to typescript - add dashboard project

---
+++ packages/client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      Naser Web page
+    
+    </div>
+  );
+}
+
+export default App;
