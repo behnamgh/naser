203244603d426814c506cf307433df159572de68
peyman shahbazi <peyman.shahbazi@zoodfood.com>
2020-03-15 16:23:10 +0330

    handling sizes

--- packages/client/src/components/sections/Header.tsx
+++ packages/client/src/components/sections/Header.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function Header({ page }: any) {
   return (
-    <div>
-      
+    <div className="section-header">
+      <video autoPlay loop muted width="320" height="240" className="homepage-video">
+        <source src="/videos/homepage.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+      </video>
     </div>
   )
 }
