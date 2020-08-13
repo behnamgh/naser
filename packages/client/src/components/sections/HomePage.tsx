864302fd2dcf8e67036f894eda694e4da81956a6
Behnam Ghafary <behnam.ghafary@gmail.com>
2020-08-13 10:41:20 +0430

    add fram for homepage background

--- packages/client/src/components/sections/HomePage.tsx
+++ packages/client/src/components/sections/HomePage.tsx
@@ -1,8 +1,9 @@
 import React, { useContext } from 'react'
 import languageContext from '../contexts/lang';
+import FRAME from '../../images/VideoFrame.png';
 
 export default function HomePage({ page }: any) {
-  const {lang} = useContext(languageContext);
+  const { lang } = useContext(languageContext);
   const videoPath = page.contents[0].values && page.contents[0].values[lang].value;
 
   return (
@@ -11,6 +12,7 @@ export default function HomePage({ page }: any) {
         <source src={videoPath || "/videos/mainpage.mp4"} type="video/mp4" />
             Your browser does not support the video tag.
       </video>}
+      <img className="header-frame" src={FRAME} alt="side-image" />
     </div>
   )
 }
