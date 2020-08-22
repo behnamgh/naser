ec279e663d85415f88663da1593670c988119d8b
BehnamGhafary <behnam.ghafary@gmail.com>
2020-08-22 20:39:33 +0430

    add duth join us

--- packages/client/src/components/sections/JoinUs.tsx
+++ packages/client/src/components/sections/JoinUs.tsx
@@ -1,16 +1,20 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, useContext } from 'react'
 // import React, { useState, useRef, useEffect } from 'react'
 
-import SOCIALHOLDER from '../../images/JoinUs/JoinUs.png';
+import SOCIALHOLDER_EN from '../../images/JoinUs/JoinUs.png';
+import SOCIALHOLDER_DE from '../../images/JoinUs/JoinUsDE.png';
 import FACEBOOK from '../../images/facebook.png';
 import INSTAGRAM from '../../images/instagram.png';
 import TWITTER from '../../images/twitter.png';
 import MAIL from '../../images/Mail.png';
 import JOINUS_BUTTON from '../../images/joinUs.png';
+import langContext from "../contexts/lang"
 
 import getHttpClient from "../../utils/httpClient";
 
 export default function JoinUs({ page }: any) {
+  const { lang } = useContext(langContext);
+
   const bodySocial = useRef<HTMLDivElement>(null);
 
   const [email, setMail] = useState("");
@@ -36,8 +40,7 @@ export default function JoinUs({ page }: any) {
   return (
     <div className="join-us">
       <div className="join-us__body">
-        <img src={SOCIALHOLDER} alt="" />
-
+        <img src={lang === "en-US" ? SOCIALHOLDER_EN : SOCIALHOLDER_DE} alt="" />
         <input className="join-us__body-input" placeholder="Your Email Address" type="text" id="mail" name="mail" value={email} onChange={handleType} />
         <div className="join-us__body-social" ref={bodySocial}>
           <a href="http://mail.com"><img src={MAIL} alt="website icon" /></a>
