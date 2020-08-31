a2188bb8d030078f412b2b04d4d163c80b6ffd78
Behnam <behnam@Desktop.Behnam>
2020-08-31 16:55:00 +0430

    remove router

--- packages/presskit/src/App.tsx
+++ packages/presskit/src/App.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from "./pages/Home"
 
 const App = () => {
 
-  return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={Home} />
-      </Switch>
-    </Router>
+  return (<Home />)
 
-  );
+  // return (
+  //   <Router>
+  //     <Switch>
+  //       <Route path="/" exact component={Home} />
+  //     </Switch>
+  //   </Router>
+
+  // );
 }
 
 export default App;
