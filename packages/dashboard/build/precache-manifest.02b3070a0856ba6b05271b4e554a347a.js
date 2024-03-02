e252f395b915d96bd7dd341edd67c51129606ea3
Behnam Ghafary <behnam.ghafary@gmail.com>
2024-03-02 12:47:45 +0100

    add build file for dashboard

---
+++ packages/dashboard/build/precache-manifest.02b3070a0856ba6b05271b4e554a347a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb44a23a5727bc7f3787a845675a518b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85b8834c71fec59ae12c",
+    "url": "/static/css/2.260df227.chunk.css"
+  },
+  {
+    "revision": "aeb953f3343715ec7a4c",
+    "url": "/static/css/main.d425d52f.chunk.css"
+  },
+  {
+    "revision": "85b8834c71fec59ae12c",
+    "url": "/static/js/2.e5d13c96.chunk.js"
+  },
+  {
+    "revision": "ca38ef27b0a23194e8ab8ec1ce759ca8",
+    "url": "/static/js/2.e5d13c96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aeb953f3343715ec7a4c",
+    "url": "/static/js/main.b6f47587.chunk.js"
+  },
+  {
+    "revision": "fef0669c63847c5d06a0",
+    "url": "/static/js/runtime-main.e9665585.js"
+  }
+]);
