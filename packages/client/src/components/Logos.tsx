e632e116e7ea8dc72e81be1304f9b7e45987d8e0
Behnam Ghafary <behnam.ghafary@gmail.com>
2024-04-07 18:05:09 +0200

    hide language selector

--- packages/client/src/components/Logos.tsx
+++ packages/client/src/components/Logos.tsx
@@ -1,23 +1,37 @@
-import React from "react";
+import React, { useContext } from "react";
 import LOGO from "../images/naser-logo.png";
 import LIGHTEN_LOGO from "../images/naser-lighten-logo.png";
-// import DE from "../images/languages/LanguageSelectDE.png";
-// import EN from "../images/languages/LanguageSelectEN.png";
-// import langContext from "../components/contexts/lang";
+import DE from "../images/languages/LanguageSelectDE.png";
+import EN from "../images/languages/LanguageSelectEN.png";
+import langContext from "../components/contexts/lang";
 
 const Logos = () => {
-  //   const { lang, toggleLanguage } = useContext(langContext);
+  const { lang, toggleLanguage } = useContext(langContext);
 
-  //   const handleLangChange = () => {
-  //     toggleLanguage();
-  //   };
+  const handleLangChange = () => {
+    toggleLanguage();
+  };
 
   return (
     <div className="logo">
       <img src={LOGO} alt="logo" />
       <img src={LIGHTEN_LOGO} alt="lighten logo" />
 
-      {/* {lang === "en-US" ? <img src={EN} alt="down" onClick={handleLangChange} /> : <img src={DE} alt="down" onClick={handleLangChange} />} */}
+      {lang === "en-US" ? (
+        <img
+          style={{ display: "none" }}
+          src={EN}
+          alt="down"
+          onClick={handleLangChange}
+        />
+      ) : (
+        <img
+          style={{ display: "none" }}
+          src={DE}
+          alt="down"
+          onClick={handleLangChange}
+        />
+      )}
     </div>
   );
 };
